a1b300735fc3fb33b2e20481eeb9c00ede6357b9
sai_kat <118158554+saikat5397@users.noreply.github.com>
2023-04-16 18:28:46 +0600

    Add files via upload

---
+++ kaj/js/custom.js
@@ -0,0 +1,12 @@
+$(function(){
+  $('.slick').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows:true,
+ prevArrow:' <i class="fa-solid fa-circle-chevron-left prev"></i>',
+    nextArrow:' <i class="fa-solid fa-circle-chevron-right next"></i>',
+  
+  });
+});
